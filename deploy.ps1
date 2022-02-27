e93e36c736a75394e6d0cfd9e1bc9bc1a62b72de
Yong Ma <yongma@microsoft.com>
2022-02-27 11:38:01 +0800

    update split sql

--- deploy.ps1
+++ deploy.ps1
@@ -3,5 +3,5 @@ Import-Module .\ym-sql-helper.ps1 -Force
 $reportFilePath = '.\deploy-report.xml'
 $sqlFilePath = '.\deploy.sql'
 
-SplitSqlPackageScript -ReportFilePath $reportFilePath -SqlFilePath $sqlFilePath
-ParallelExecAllScript -ConnString 'asdasdasd' -Parallelcount 4
+#SplitSqlPackageScript -ReportFilePath $reportFilePath -SqlFilePath $sqlFilePath
+ParallelExecAllScript -ConnString 'Server=,,1433;Initial Catalog=TestDB;Persist Security Info=False;User ID=sa;Password=password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;' -Parallelcount 4
--- ym-sql-helper.ps1
+++ ym-sql-helper.ps1
@@ -5,10 +5,76 @@ function SplitSqlPackageScript {
     )
 
     [xml]$report = Get-Content $ReportFilePath
+    $createTableAsSelects = ($report.DeploymentReport.Operations.Operation | Where-Object { $_.name -eq 'CreateTableAsSelect' }).Item.Value
+    $tableRebuilds = ($report.DeploymentReport.Operations.Operation | Where-Object { $_.name -eq 'TableRebuild' }).Item.Value
+
     $script = Get-Content $SQLFilePath
-    
-    SplitSqlForTableRebuild -ReportContent $report -ScriptContent $script
-    SplitSqlForCreateTableAsSelect -ReportContent $report -ScriptContent $script
+
+    SplitSql -ScriptContent $script -TableNames $createTableAsSelects -TargetPath .\TableAsSelectSQL
+    SplitSql -ScriptContent $script -TableNames $tableRebuilds -TargetPath .\TableRebuildSQL
+}
+
+Function SplitSql {
+    param (
+        $ScriptContent,
+        $TableNames,
+        $TargetPath
+    )
+
+    If (Test-Path $TargetPath) {
+        Remove-Item $TargetPath -Force -Recurse
+    }
+    New-Item $TargetPath -Type directory -Force
+
+    foreach ($table in $TableNames) {
+        Write-Host $table 'is be extracted'
+
+        $tableSQL = GetSQLScriptHead($ScriptContent);
+        $tableEscape = $table.Replace('[', '\[').Replace(']', '\]').Replace('.', '\.')
+
+        $beginMatch = 0
+        for ($i = 41; $i -lt $ScriptContent.Length; $i = $i + 1) {
+
+            if ($beginMatch -gt 0) {
+                $tableSQL += $ScriptContent[$i]
+            }
+            else {
+
+                $hit = $false
+                switch ($ScriptContent[$i]) {
+                    { $_ -match ('PRINT N''Create Table as Select on ' + $tableEscape + '') } { $hit = $true }
+                    { $_ -match ('PRINT N''Creating Primary Key unnamed constraint on ' + $tableEscape + '') } { $hit = $true }
+                    { $_ -match ('PRINT N''Starting rebuilding table ' + $tableEscape + '') } { $hit = $true }
+                }
+
+                if ($hit) {
+                    $tableSQL += $ScriptContent[$i]
+                    $beginMatch = 2
+                }
+            }
+
+            if ( $ScriptContent[$i] -match 'GO') {
+                $beginMatch -= 1;
+            }
+        }
+
+        $tableName = ([regex]::Matches($table , '(?<=\[)(.*?)(?=\])'))[1].Value
+        Set-Content -value  $tableSQL -Encoding unicode  -LiteralPath  ($TargetPath + '\' + $tableName + '.sql')
+    }
+}
+
+Function GetSQLScriptHead {
+    param (
+        $ScriptContent
+    )
+
+    $result = @()
+
+    foreach ($i in 0..40) {
+        $result += $ScriptContent[$i]
+    }
+
+    return $result
 }
 
 Function ParallelExecAllScript {
@@ -21,91 +87,6 @@ Function ParallelExecAllScript {
     ParallelExecSQL -TableSQLFilePath .\TableAsSelectSQL\ -ConnString $ConnString -Parallelcount $Parallelcount
 }
 
-Function SplitSqlForCreateTableAsSelect {
-    param (
-        $ReportContent,
-        $ScriptContent
-    )
-    $createTableAsSelects = ($ReportContent.DeploymentReport.Operations.Operation | Where-Object { $_.name -eq 'CreateTableAsSelect' }).Item.Value
-
-    $directory = '.\TableAsSelectSQL'
-    If (Test-Path $directory) {
-        Remove-Item $directory -Force -Recurse
-    }
-    New-Item $directory -Type directory -Force
-
-    foreach ($table in $createTableAsSelects) {
-        Write-Host $table 'is create table as selects'
-
-        $tableSQL = @()
-        $beginMatch = $false;
-        $tableEscape = $table.Replace('[', '\[').Replace(']', '\]').Replace('.', '\.')
-
-        for ($i = 1; $i -lt $ScriptContent.Length; $i = $i + 1) {
-
-            if ($beginMatch) {
-               
-                if ( $ScriptContent[$i] -match 'PRINT') {
-                    $beginMatch = $false;
-                    break
-                }
-
-                $tableSQL += $ScriptContent[$i]
-            }
-
-            if (!$beginMatch -and ($ScriptContent[$i] -match ('PRINT N''Create Table as Select on ' + $tableEscape + ''))) {
-                $tableSQL += $ScriptContent[$i]
-                $beginMatch = $true
-            }
-        }
-
-        $tableName = ([regex]::Matches($table , '(?<=\[)(.*?)(?=\])'))[1].Value
-        Set-Content -value  $tableSQL -Encoding unicode  -LiteralPath  ($directory + '\' + $tableName + '.sql')
-    }
-}
-
-Function SplitSqlForTableRebuild {
-    param (
-        $ReportContent,
-        $ScriptContent
-    )
-    $tableRebuilds = ($ReportContent.DeploymentReport.Operations.Operation | Where-Object { $_.name -eq 'TableRebuild' }).Item.Value
-
-    $directory = '.\TableRebuildSQL'
-    If (Test-Path $directory) {
-        Remove-Item $directory -Force -Recurse
-    }
-    New-Item $directory -Type directory -Force
-
-    foreach ($table in $tableRebuilds) {
-        Write-Host $table 'is rebuilding table'
-
-        $tableSQL = @()
-        $beginMatch = $false;
-        $tableEscape = $table.Replace('[', '\[').Replace(']', '\]').Replace('.', '\.')
-
-        for ($i = 1; $i -lt $ScriptContent.Length; $i = $i + 1) {
-
-            if ($beginMatch) {
-
-                if ( $ScriptContent[$i] -match 'PRINT') {
-                    $beginMatch = $false;
-                    break
-                }
-
-                $tableSQL += $ScriptContent[$i]
-            }
-
-            if (!$beginMatch -and ($ScriptContent[$i] -match ('PRINT N''Starting rebuilding table ' + $tableEscape + ''))) {
-                $tableSQL += $ScriptContent[$i]
-                $beginMatch = $true
-            }
-        }
-
-        $tableName = ([regex]::Matches($table , '(?<=\[)(.*?)(?=\])'))[1].Value
-        Set-Content -value  $tableSQL -Encoding unicode  -LiteralPath  ($directory + '\' + $tableName + '.sql')
-    }
-}
 Function ParallelExecSQL {
     param (
         [string]$TableSQLFilePath,
@@ -131,27 +112,32 @@ Function ParallelExecSQL {
             [string] $file, 
             [string] $connString
         )
-        #Write-Host $file
-        #Write-Host $connString
+        $completed = $true
 
         try { 
             Invoke-Sqlcmd -connectionString $connString -Inputfile $file
         }
-        catch { 
-            Write-Host "An error occurred." -ForegroundColor Red
+        catch {
+            $completed = $false 
+            Write-Warning "$file has an error occurred. $Error"
         }
-        Write-Host "$file Completed" -ForegroundColor Yellow
+
+        if ($completed) {
+            Write-Host "$file Completed"
+        }
     };
 
     foreach ($i in 1..$Parallelcount) {
 
-        Start-Job -ScriptBlock $task -Name "task$i" -ArgumentList $files[$i - 1].FullName, $ConnString
+        $job = Start-Job -ScriptBlock $task -Name "task$i" -ArgumentList $files[$i - 1].FullName, $ConnString
     }
 
     $nextIndex = $Parallelcount
     
     while (($nextIndex -lt $files.Length) -or ($taskCount -gt 0)) {
-        foreach ($job in Get-Job) {
+
+        $jobs = Get-Job
+        foreach ($job in $jobs) {
             $state = [string]$job.State
             if (($state -eq "Completed") -or ($state -eq "Failed")) {
                 Receive-Job -Job $job   
@@ -159,7 +145,7 @@ Function ParallelExecSQL {
                 $taskCount--
                 if ($nextIndex -lt $files.Length) {   
                     $taskNumber = $nextIndex + 1
-                    Start-Job -ScriptBlock $task -Name "task$taskNumber" -ArgumentList $files[$nextIndex].FullName, $connString
+                    $job = Start-Job -ScriptBlock $task -Name "task$taskNumber" -ArgumentList $files[$nextIndex].FullName, $connString
                     $nextIndex++
                 }
             }
